231a7749ce63979ff8982fb319478c842492bb3a
Kohei Matsubara <matsubygh@gmail.com>
2019-05-03 17:17:39 +0900

    init LoopBack app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sample-loopback
-api sample that created by loopback(ver 4)
+
+[![LoopBack](https://github.com/strongloop/loopback-next/raw/master/docs/site/imgs/branding/Powered-by-LoopBack-Badge-(blue)-@2x.png)](http://loopback.io/)
---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import { SampleLoopbackApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
+export { SampleLoopbackApplication };
+export declare function main(options?: ApplicationConfig): Promise<SampleLoopbackApplication>;
