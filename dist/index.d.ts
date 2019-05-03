7932930415d7ba1cc2166cff82d674d787f7f573
Kohei Matsubara <matsubygh@gmail.com>
2019-05-03 18:00:34 +0900

    move sqlite database file and remove distribution from git

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { SampleLoopbackApplication } from './application';
-import { ApplicationConfig } from '@loopback/core';
-export { SampleLoopbackApplication };
-export declare function main(options?: ApplicationConfig): Promise<SampleLoopbackApplication>;
--- dist/models/user.model.d.ts
+++
@@ -1,8 +0,0 @@
-import { Entity } from '@loopback/repository';
-export declare class User extends Entity {
-    id?: number;
-    firstName: string;
-    lastName: string;
-    age?: number;
-    constructor(data?: Partial<User>);
-}
